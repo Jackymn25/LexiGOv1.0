60322022f29fe30674fa64a07e594c702b54529f
Y0m1ya <jacobke7767@gmail.com>
2025-07-26 22:31:40 -0400

    Delete src/main/java/entity/PersonalProfileFactory.java
    delete wrong file

--- src/main/java/entity/PersonalProfileFactory.java
+++
@@ -1,8 +0,0 @@
-package entity;
-
-public class PersonalProfileFactory implements ProfileFactory {
-    @Override
-    public PersonalProfile createPersonalProfile(String username, String language) {
-        return new PersonalProfile(username, language);
-    }
-}
